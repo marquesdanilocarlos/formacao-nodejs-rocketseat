7ac31f3269e78626e8848855caa21f805bafe93f
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-04-30 20:27:42 -0300

    Aula 02 - Fundamentos de métodos HTTP e recursos da URL

--- 01-fundamentos/src/server.ts
+++ 01-fundamentos/src/server.ts
@@ -1,6 +1,16 @@
 import http, {IncomingMessage, Server, ServerResponse} from 'node:http';
 
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse): ServerResponse => {
+    const  {method, url} = req;
+
+    if (method === 'GET' && url === '/users') {
+        return res.end('Listagem de usuários');
+    }
+
+    if (method === 'POST' && url === '/users') {
+        return res.end('Criação de usuários');
+    }
+
     return res.end('Hello World!');
 });
 
