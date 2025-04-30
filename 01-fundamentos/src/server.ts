a22d3a1590ca1c0dbf515553ee34b34297da4c81
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-04-30 20:43:47 -0300

    Aula 03 - Salvamento de dados em memória e cabeçalho tipo de conteúdo no response

--- 01-fundamentos/src/server.ts
+++ 01-fundamentos/src/server.ts
@@ -1,13 +1,22 @@
 import http, {IncomingMessage, Server, ServerResponse} from 'node:http';
 
+const users: Array<Object> = [];
+
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse): ServerResponse => {
     const  {method, url} = req;
 
     if (method === 'GET' && url === '/users') {
-        return res.end('Listagem de usuários');
+        return res
+            .setHeader('Content-Type', 'application/json')
+            .end(JSON.stringify(users));
     }
 
     if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'Diego',
+            email: '5P6mT@example.com'
+        });
         return res.end('Criação de usuários');
     }
 
