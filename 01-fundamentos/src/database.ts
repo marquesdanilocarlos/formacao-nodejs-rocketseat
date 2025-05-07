2b7f9ad98fe52812ba1ec3928bc02046bfab6c88
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-05-06 22:33:59 -0300

    Aula 13 - Persistencia de dados em arquivo JSON

--- 01-fundamentos/src/database.ts
+++ 01-fundamentos/src/database.ts
@@ -1,17 +1,40 @@
-export default class Database {
-    #database: any = {};
+import fs from 'node:fs/promises';
 
-    select(table: string): object[] {
+interface DB {
+    users?: object[];
+}
+
+export default class Database {
+    #database: DB = {};
+    #dbPath: URL  = new URL('../db.json', import.meta.url);
+
+    constructor() {
+        fs.readFile(this.#dbPath, 'utf-8').then(data => {
+            this.#database = JSON.parse(data);
+        }).catch(
+            () => {
+                this.#persist();
+            }
+        )
+    }
+
+    #persist() {
+        console.log(this.#dbPath);
+        fs.writeFile(this.#dbPath, JSON.stringify(this.#database));
+    }
+
+    select<K extends keyof DB>(table: K): object[] {
         return this.#database[table] ?? [];
     }
 
-    insert(table: string, data: object): object | undefined {
+    insert<K extends keyof DB>(table: K, data: object): object | undefined {
         if (Array.isArray(this.#database[table])){
             this.#database[table].push(data);
             return;
         }
 
         this.#database[table] = [data];
+        this.#persist();
 
         return data;
     }
