d952e47aef3fce47df120d83593b596a2029504a
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-06-19 13:15:40 -0300

    Aula 26 - Configuração de variáveis de ambiente

--- 03-api-rest/src/database.ts
+++ 03-api-rest/src/database.ts
@@ -1,15 +1,20 @@
-import knex, { Knex } from 'knex'
+import 'dotenv/config';
+import knex, { Knex } from 'knex';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined');
+}
 
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './database/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
     directory: './database/migrations',
   },
-}
+};
 
-export const knexInstance = knex(config)
+export const knexInstance = knex(config);
