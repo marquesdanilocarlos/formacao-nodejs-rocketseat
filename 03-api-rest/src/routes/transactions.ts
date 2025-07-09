dffcc107d9f1796407d64552c07117d8811b8f29
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-07-08 22:16:18 -0300

    Aula 31 - Listagem de transactions e obtenção de transaction específica

--- 03-api-rest/src/routes/transactions.ts
+++ 03-api-rest/src/routes/transactions.ts
@@ -3,6 +3,23 @@ import { z } from 'zod';
 import { knexInstance } from '@/database';
 
 export default async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async (request: FastifyRequest, response: FastifyReply) => {
+    const transactions = await knexInstance('transactions').select('*');
+
+    return response.status(200).send(transactions);
+  });
+
+  app.get('/:id', async (request: FastifyRequest, response: FastifyReply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(request.params);
+    const transaction = await knexInstance('transactions').where('id', id).first();
+
+    return response.status(200).send(transaction);
+  });
+
   app.post('/', async (request: FastifyRequest, response: FastifyReply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
