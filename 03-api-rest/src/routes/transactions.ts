9c89592a0d0dc5434607337e5e32894c5c1fa929
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-07-09 13:22:06 -0300

    Aula 35 - Middleware global para checar existência do Cookie

--- 03-api-rest/src/routes/transactions.ts
+++ 03-api-rest/src/routes/transactions.ts
@@ -3,13 +3,14 @@ import TransactionsController from '@/controllers/Transactions';
 import checkSessionId from '@/middlewares/checkSessionId';
 
 export default async function transactionsRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', checkSessionId);
   const transactionsController = new TransactionsController();
 
-  app.get('/', { preHandler: [checkSessionId] }, transactionsController.index);
+  app.get('/', transactionsController.index);
 
-  app.get('/:id', { preHandler: [checkSessionId] }, transactionsController.get);
+  app.get('/:id', transactionsController.get);
 
-  app.get('/summary', { preHandler: [checkSessionId] }, transactionsController.summary);
+  app.get('/summary', transactionsController.summary);
 
   app.post('/', transactionsController.create);
 }
