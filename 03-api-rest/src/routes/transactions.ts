fad263ab964d03678f0f82fffefad4f8d63f469c
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-07-08 21:35:32 -0300

    Aula 29 - Método de criação de nova transaction

--- 03-api-rest/src/routes/transactions.ts
+++ 03-api-rest/src/routes/transactions.ts
@@ -1,13 +1,23 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
 import { knexInstance } from '@/database';
 
 export default async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knexInstance('transactions')
-      .select('*')
-      .where('amount', '>', 100)
-      .groupBy('id');
+  app.post('/', async (request: FastifyRequest, response: FastifyReply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    });
 
-    return { transaction };
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body);
+
+    await knexInstance('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    });
+
+    return response.status(201).send();
   });
 }
