92fefc8c6d5e5db52afb4b610f04e67fc7106e4b
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-07-10 01:00:35 -0300

    Aula 39 - Teste de listagem das transactions

--- 03-api-rest/test/transactions.test.ts
+++ 03-api-rest/test/transactions.test.ts
@@ -23,4 +23,45 @@ describe('Transactions routes', () => {
 
     expect(response.statusCode).toBe(201);
   });
+
+  test('list all transactions', async () => {
+    const createResponse = await app.inject({
+      method: 'POST',
+      url: '/transactions',
+      payload: {
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      },
+    });
+
+    const setCookie = createResponse.headers['set-cookie'];
+
+    const sessionCookie = Array.isArray(setCookie)
+      ? setCookie.find((c) => c.startsWith('sessionId='))
+      : setCookie?.startsWith('sessionId=')
+        ? setCookie
+        : undefined;
+
+    expect(sessionCookie).toBeDefined();
+
+    const response = await app.inject({
+      method: 'GET',
+      url: '/transactions',
+      headers: {
+        Cookie: sessionCookie,
+      },
+    });
+
+    console.log(typeof response.body);
+
+    expect(response.json()).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          title: 'New transaction',
+          amount: 5000,
+        }),
+      ])
+    );
+  });
 });
