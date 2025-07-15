0c4fd47e68b5be5853c5640ac4268dd43d8c8f0b
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-07-14 23:52:33 -0300

    Aula 43 - Configurações para produção

--- 03-api-rest/src/validation/env.ts
+++ 03-api-rest/src/validation/env.ts
@@ -11,6 +11,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
 });
 
 const _env = envSchema.safeParse(process.env);
