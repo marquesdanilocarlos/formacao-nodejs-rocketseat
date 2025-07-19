b78a158821930a02a3546e21708a93efee641182
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-07-19 13:25:00 -0300

    Aula 46 - Definição e validação das variáveis de ambiente com Zod

--- 04-desafio-2/src/server.ts
+++ 04-desafio-2/src/server.ts
@@ -1,9 +1,10 @@
 import app from '@/app';
+import env from '@/validation/env';
 
 app
   .listen({
-    port: 3000,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port ${env.PORT}`);
   });
---
+++ 04-desafio-2/src/validation/env.ts
@@ -0,0 +1,11 @@
+import z from 'zod';
+import 'dotenv/config';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+  PORT: z.coerce.number().default(3000),
+});
+
+const env = envSchema.parse(process.env);
+
+export default env;
