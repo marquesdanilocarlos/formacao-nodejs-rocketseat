ed955958526f0e11841b111352936eb4b74dbb7a
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-07-23 23:08:36 -0300

    Aula 66 - Instalação do Prisma

--- 05-api-solid/src/app.ts
+++ 05-api-solid/src/app.ts
@@ -1,5 +1,8 @@
 import fastify, { FastifyInstance } from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app: FastifyInstance = fastify()
+
+const prisma = new PrismaClient()
 
 export default app
